5a69bb1f94f4c8519a5f9bde87f78f24c194c180
Kelsey Katie Sara Anethesia <pcvoxels@gmail.com>
2021-07-05 22:41:41 +0800

    idk whats happening xD

--- Ass1_WebDev/main_folder/js_files/js_scripts.js
+++ Ass1_WebDev/main_folder/js_files/js_scripts.js
@@ -1,5 +1,8 @@
 function dark_mode_toggle() {
     var element = document.body;
-    link.href = '..\css_files\css_id.css';
     element.classList.toggle("light-mode");
+    color: #000000;
+
+    var button = document.getElementById('background-color').style.backgroundColor;  
+    var color = '#ffffff';
 }
