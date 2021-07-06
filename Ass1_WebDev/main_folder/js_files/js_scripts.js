081d7055c37b54b33b7ddc63904f3b7207e4e614
Kelsey Katie Sara Anethesia <pcvoxels@gmail.com>
2021-07-06 13:28:38 +0800

    big changes to index page
    + a top Nav bar and a logo
    + header for site and a clickable link
    
    ! transition effects with images

--- Ass1_WebDev/main_folder/js_files/js_scripts.js
+++ Ass1_WebDev/main_folder/js_files/js_scripts.js
@@ -1,7 +1,6 @@
 function dark_mode_toggle() {
     var element = document.body;
     element.classList.toggle("light-mode");
-    color: #000000;
 
     var button = document.getElementById('background-color').style.backgroundColor;  
     var color = '#ffffff';
